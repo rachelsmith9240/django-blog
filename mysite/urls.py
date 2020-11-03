c850b17be6e674d986e350553b2623ea8f38a0ef
rachelsmith9240 <rachelsmith9240@gmail.com>
2020-11-02 21:11:47 -0800

    initial polling setup done but throws error when rendered online

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from polling.views import list_view, detail_view
 
 urlpatterns = [
+    path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
 ]
