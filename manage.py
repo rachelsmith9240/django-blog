078fcdb839790c51fb9283d21be04eb93f2fc12c
rachelsmith9240 <rachelsmith9240@gmail.com>
2020-11-21 19:26:10 -0800

    Fixing some syntax errors

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
@@ -13,8 +12,7 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+            "forget to activate a virtual environment?")
     execute_from_command_line(sys.argv)
 
 
